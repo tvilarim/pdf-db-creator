c802056a48c88d612839ffae327e4ed2ad8e3659
tvilarim <tvilarim@msn.com>
2024-09-15 16:51:26 -0300

    version: v1.3

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,7 @@
 
 docker build -t tvilarim/pdf-db-creator:latest . &&
 docker push tvilarim/pdf-db-creator:latest &&
+docker pull tvilarim/pdf-db-creator:latest &&
 
 kubectl apply -f k8s/deployment.yaml &&
 kubectl apply -f k8s/service.yaml &&
