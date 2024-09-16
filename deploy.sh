7db151b1e923a0989afc7ce5abfb212ba66cf44d
tvilarim <tvilarim@msn.com>
2024-09-16 14:28:08 -0300

    v1.25 # redis - ajustes no projeto todo

--- deploy.sh
+++ deploy.sh
@@ -4,8 +4,8 @@ docker build -t tvilarim/pdf-db-creator:latest . &&
 docker push tvilarim/pdf-db-creator:latest &&
 docker pull tvilarim/pdf-db-creator:latest &&
 
-kubectl apply -f redis-deployment.yaml &&
-kubectl apply -f celery-worker-deployment.yaml &&
-kubectl apply -f pdf-db-creator-deployment.yaml &&
-kubectl apply -f pdf-db-creator-service.yaml &&
+kubectl apply -f k8s/redis-deployment.yaml &&
+kubectl apply -f k8s/celery-worker-deployment.yaml &&
+kubectl apply -f k8s/pdf-db-creator-deployment.yaml &&
+kubectl apply -f k8s/pdf-db-creator-service.yaml &&
 kubectl apply -f k8s/hpa.yaml
