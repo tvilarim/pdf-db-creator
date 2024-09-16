209f7e04dbf6e8cfb834b1e85fb6cfbfa4fd9203
tvilarim <tvilarim@msn.com>
2024-09-16 14:16:25 -0300

    v1.24

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,8 @@ docker build -t tvilarim/pdf-db-creator:latest . &&
 docker push tvilarim/pdf-db-creator:latest &&
 docker pull tvilarim/pdf-db-creator:latest &&
 
-kubectl apply -f k8s/deployment.yaml &&
-kubectl apply -f k8s/service.yaml &&
+kubectl apply -f redis-deployment.yaml &&
+kubectl apply -f celery-worker-deployment.yaml &&
+kubectl apply -f pdf-db-creator-deployment.yaml &&
+kubectl apply -f pdf-db-creator-service.yaml &&
 kubectl apply -f k8s/hpa.yaml
